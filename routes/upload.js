fdbe9c3acff04a3ebf3b4416f423eef3d88c2d50
Starlin de la cruz <starlinr82@gmail.com>
2019-07-12 00:23:39 -0400

    Correciones

--- routes/upload.js
+++ routes/upload.js
@@ -5,18 +5,18 @@ const EASYIMAGE = require('easyimage')
 
 const storage = multer.diskStorage({
     destination: './public/uploads',
-    filename: function (req, file, cb) {        
+    filename: function(req, file, cb) {
         // null as first argument means no error
         let remane = file.originalname.split('-')
 
-        const archivo =  `${ new Date().getTime()}-${remane[1]}-${remane[2]}`
+        const archivo = `${ new Date().getTime()}-${remane[1]}-${remane[2]}`
         cb(null, archivo)
     }
 })
 
 const upload = multer({
-    storage: storage, 
-    fileFilter: function (req, file, cb) {
+    storage: storage,
+    fileFilter: function(req, file, cb) {
         sanitizeFile(file, cb);
     }
 }).any()
@@ -26,86 +26,83 @@ var fs = require('fs');
 /* POST saveblog router. */
 router.post('/saveBlog', async function(req, res, next) {
 
-    upload (req, res,async (err) => {
-  
-        if (err){ 
+    upload(req, res, async(err) => {
+
+        if (err) {
             return res.status(500).json({
                 ok: false
             })
-        }else{
+        } else {
             // If file is not selected
-            if (req.files.length === 0 ||  req.files === undefined) {
+            if (req.files.length === 0 || req.files === undefined) {
                 return res.status(400).json({
                     ok: false
                 })
             } else {
-                console.log(req.body);
-                console.log(req.files);
-                
-                // const Imagenes = [];
-                // for(imagen of req.files){
-                //     let normal
-                //     try {
-                //         normal = await GeneralImagen(imagen, 600, req )
-                //         const thumbs = await GeneralImagen(imagen, 200 ,req)
-                        
-                //         Imagenes.push({
-                //             img: `http://localhost:3000${normal.replace('public','')}`,
-                //             thumb: `http://localhost:3000${thumbs.replace('./public','')}`
-                //         })
-                //     } catch (error) {
-                //         await fs.unlinkSync(imagen.path)
-                //         if(error.height === 200) await fs.unlinkSync(normal)
-                //     }
-                    
-                // }
-                
-                
-                // res.status(200).json({
-                //     msg: 'File uploaded successfully!',
-                //     imagen: Imagenes
-                // })
+                const Imagenes = [];
+                for (imagen of req.files) {
+                    let normal
+                    try {
+                        normal = await GeneralImagen(imagen, 600, req)
+                        const thumbs = await GeneralImagen(imagen, 200, req)
+
+                        Imagenes.push({
+                            img: `http://localhost:3000${normal.replace('public','')}`,
+                            thumb: `http://localhost:3000${thumbs.replace('./public','')}`
+                        })
+                    } catch (error) {
+                        await fs.unlinkSync(imagen.path)
+                        if (error.height === 200) await fs.unlinkSync(normal)
+                    }
+
+                }
+
+
+                res.status(200).json({
+                    imagen: Imagenes
+                })
             }
         }
     })
 });
 
 function GeneralImagen(imagen, height, req) {
-    return new Promise(async (resolve, reject) => {
+    return new Promise(async(resolve, reject) => {
         thumbnailInfo = null
         try {
-             let width
-            
+            let width
+
             const imageInfo = await EASYIMAGE.info(imagen.path);
 
             if (imageInfo.height > height) {
                 let heightTemp = imageInfo.height - height
-                 
+
                 let porcientoHeight = await generalPorcentaje(heightTemp, imageInfo.height)
-                
-                width = imageInfo.width - ((porcientoHeight * imageInfo.width)/100) 
-                // let  x = porcientoHeight * imageInfo.width
+
+                width = imageInfo.width - ((porcientoHeight * imageInfo.width) / 100)
+                    // let  x = porcientoHeight * imageInfo.width
 
             } else {
 
                 let heightTemp = imageInfo.height + height
-                 
-                let porcientoHeight = await generalPorcentaje(height, heightTemp)
-                
-                width = imageInfo.width + ((porcientoHeight * imageInfo.width)/100) 
-            }
 
-            let destino = 'Normal' 
-            if( height === 200 ) {
-                let destino = 'Thumbnail' 
+                let porcientoHeight = await generalPorcentaje(height, heightTemp)
+
+                width = imageInfo.width + ((porcientoHeight * imageInfo.width) / 100)
             }
-             thumbnailInfo = await EASYIMAGE.thumbnail({
+            console.log(req.body);
+
+            let destino = `Normal/${new Date().getTime()}.${imageInfo.name.split('.')[1].toLowerCase()}`
+            if (height === 200) {
+                let destino = `Thumbnail/${new Date().getTime()}-thumb.${imageInfo.name.split('.')[1].toLowerCase()}`
+            }
+            thumbnailInfo = await EASYIMAGE.thumbnail({
                 src: imagen.path,
-                dst: `./public/thumbnail/${new Date().getTime()}-thumb.${imageInfo.name.split('.')[1].toLowerCase()}`,
-                width:  width,
+                dst: `./public/thumbnail/}`,
+                width: width,
                 height: height,
             });
-            
+
             resolve(thumbnailInfo.path)
         } catch (e) {
             reject({
@@ -122,27 +119,26 @@ function GeneralImagen(imagen, height, req) {
 
 function generalPorcentaje(cantidad, total) {
     return new Promise((resolve, reject) => {
-        let porciento = cantidad/total;
+        let porciento = cantidad / total;
         porcentaje = porciento * 100
-        
+
         resolve(porcentaje)
     })
 }
 
 function sanitizeFile(file, cb) {
     // Define the allowed extension
-    let fileExts = ['png', 'jpg', 'jpeg', 'gif','ico']
-    // Check allowed extensions
+    let fileExts = ['png', 'jpg', 'jpeg', 'gif', 'ico']
+        // Check allowed extensions
     let ext = file.originalname.split('.')
     let isAllowedExt = fileExts.includes(file.originalname.split('.')[ext.length - 1].toLowerCase());
     // Mime type must be an image
     let isAllowedMimeType = file.mimetype.startsWith("image/")
     if (isAllowedExt && isAllowedMimeType) {
         return cb(null, true) // no errors
-    }
-    else {
+    } else {
         // pass error msg to callback, which can be displaye in frontend
-        cb({ok: false,error: "su dita madre"})
+        cb({ ok: false, error: "su dita madre" })
     }
 }
 
